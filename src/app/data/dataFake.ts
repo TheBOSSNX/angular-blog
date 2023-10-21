99fabb0cfa8db824348318a683caa73ce312b65a
TheBOSSNX <alcarvalho3301@gmail.com>
2023-10-21 14:03:32 -0400

    Games

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    "title": "SEQUÊNCIA DE JEDI SURVIVAL",
+    "description": "Rumores de que a EA Games está desenvolvendo uma sequência de Star Wars: Jedi Survival.",
+    "photoCover":"https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/927f8932b40a5edadba68ef7a1435a36.jpg"
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+    "title": "Starfield é um sucesso ou fracasso?",
+    "description": "A grande expectativa criada em torno desse jogo deu o que falar após o seu lançamento.",
+    "photoCover":"https://uploads.jovemnerd.com.br/wp-content/uploads/2023/08/starfield_capa__00130w-1210x544.jpg"
+  },
+  {
+    "id":"3",
+    "title": "The Evil Within é o que Resident Evil deveria ter se tornado!",
+    "description": "É o que muitos amantes do Survival Horror pensam, pelo fato do jogo ter sido criado pelo mesmo criador dos jogos da franquia tão amada da Capcom.",
+    "photoCover":"https://cdn1.epicgames.com/offer/d602fd70876141ba9bf4d54809c423d1/EGS_TheEvilWithin_TangoGameworks_S1_2560x1440-16b7f817d8803517fbecbf3f175ac997"
+  },
+  {
+    "id":"4",
+    "title": "GOTY VINDO POR AÍ? É o que os fâs acham sobre o novo Zelda",
+    "description": "Apesar de parecer apenas uma expansão do seu antecessor, The Legend of Zelda: TOTK tem grandes chances de ganhar o GOTY, afirmam fãs.",
+    "photoCover":"https://s2-techtudo.glbimg.com/_tmRjoylhidOdOVj6fSxur2q5mM=/540x304/top/smart/https://i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2022/C/w/0c866sTNGjWkOsg4TBfw/1dca6b02-2534-40f7-866a-ae1abc7c9a66.jpg"
   }
 ]
